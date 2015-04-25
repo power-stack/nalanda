508e9180435b68f912097bc81d0e8fc7e7d327a6
gengjh <david.geng@idevops.net>
2015-04-25 09:27:27 +0800

    Support pipework SDN.
    Assign real IP address from dhcp when run docker container.

---
+++ mesos/entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [[ -n $MULTICLOUD_DNS && -n MULTICLOUD_DNS_SEARCH ]] ; then
+  echo "Replacing /etc/resolv.conf with:"
+  echo "nameserver $MULTICLOUD_DNS" 
+  echo "search $MULTICLOUD_DNS_SEARCH"
+  echo "nameserver $MULTICLOUD_DNS" > /etc/resolv.conf
+  echo "search $MULTICLOUD_DNS_SEARCH" >> /etc/resolv.conf
+fi;
+
+echo "Taking a nap, to allow weave network to properly set up.."
+sleep 10
+if [[ -n $CONTAINER_IF ]]; then
+  echo "Lookup ip addr from container interface: $CONTAINER_IF .."
+  until [ -n "$container_ip" ]
+  do
+    container_ip=$(ip addr show dev $CONTAINER_IF | grep 'inet ' | awk '{print $2}')
+    sleep 2
+  done
+  echo "Container IP addr from SDN: $container_ip .."
+  export MESOS_IP=$(echo "$container_ip" |awk 'BEGIN { FS = "/" }; { print $1 }')
+fi;
+
+echo "That was a good nap. Now to work..."
+exec "$@"
